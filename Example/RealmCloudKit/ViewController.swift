1fdafc367399e8f59d1022acb91b3ec646d94178
StuFF mc <mc@stuffmc.com>
2016-01-25 12:24:51 +0100

    Basic test of a Model with a date

--- Example/RealmCloudKit/ViewController.swift
+++ Example/RealmCloudKit/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import RealmSwift
+import RealmCloudKit
+import CloudKit
+
+class Test: Object {
+  dynamic var date = Test.stringDate
+  
+  static var stringDate: String = {
+    return "\(NSDate())"
+  }()
+}
 
 class ViewController: UIViewController {
+    var realm = try! Realm()
+    let publicDB = CKContainer.defaultContainer().publicCloudDatabase
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      print(realm.path)
+        try! realm.write {
+          realm.add(Test(value: [Test.stringDate]))
+        }
+      
+        print(realm.objects(Test))
+
+        let query = CKQuery(recordType: "Test", predicate: NSPredicate(value: true))
+        publicDB.performQuery(query, inZoneWithID: nil) { (records, error) -> Void in
+          print(records)
+          print(error)
+        }
+      
+        // Uncomment the following and see a wonderful Infinite Loop :-) Why?
+        RealmCloudKit.start(realmToBeSynced: realm) { (resultRealm, error) -> Void in
+          print(resultRealm!.path)
+          print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
